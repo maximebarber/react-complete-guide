8f1c590fbecf08aaf4ab072362aeb3902e891805
Maxime <maxime@processmaker.fr>
2019-09-13 13:39:44 +0200

    50

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
-
 import './App.css';
 import Person from './Person/Person';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
 
 class App extends Component {
   state = {
@@ -12,88 +9,58 @@ class App extends Component {
       { name: 'Manu', age: 29 },
       { name: 'Stephanie', age: 26 }
     ],
-    hives: [
-      {name: 'First class!'},
-      {name: 'Bee Gees'},
-      {name: 'Valérie'},
-    ],
     otherState: 'some other value'
-  };
-
-  newUserInputHandler = (event) => {
-    this.setState({
-      hives: [
-        { name: event.target.value },
-        /* { name: 'Bee Gees' },
-        { name: 'Valérie' }, */
-      ]
-    })
   }
 
   switchNameHandler = (newName) => {
     // console.log('Was clicked!');
     // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    this.setState( {
       persons: [
         { name: newName, age: 28 },
         { name: 'Manu', age: 29 },
         { name: 'Stephanie', age: 27 }
       ]
-    });
-  };
+    } )
+  }
 
   nameChangedHandler = (event) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
+    this.setState( {
       persons: [
         { name: 'Max', age: 28 },
         { name: event.target.value, age: 29 },
         { name: 'Stephanie', age: 26 }
       ]
-    });
-  };
+    } )
+  }
 
-  render() {
+  render () {
     const style = {
       backgroundColor: 'white',
       font: 'inherit',
-      border: '1px solid seablue',
+      border: '1px solid blue',
       padding: '8px',
       cursor: 'pointer'
-    }
+    };
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <h3>Exercice 1</h3>
-        <UserInput 
-          value={this.state.hives[0].name}
-          changed={this.newUserInputHandler}
-        />
-        <UserOutput name={this.state.hives[0].name}/>
-        <h3>Lesson</h3>
         <button 
           style={style}
-          onClick={() => this.switchNameHandler('Maximilian')}>Switch Name
-        </button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
           age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Maxou')}
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
